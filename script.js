c1c43ca1b1ba7dba36ed661fe017106fcfea338c
ValeVargasR0 <155971113+ValeVargasR0@users.noreply.github.com>
2025-02-15 00:23:00 -0400

    changed it back
    the changes did not work, I had to chaged them back to what they were, some other functions stopped working due to the change.

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ const nextLevel = () => {
   level++;
   document.getElementById("level").innerText = level;
   playerSequence = [];
-  colorSequence.push(colors[Math.floor(Math.random() * colors)]);
+  colorSequence.push(colors[Math.floor(Math.random() * 4)]);
   playSequence();
 };
 /*Funcion que permite que se avance de nivel, agregando un nuevo color a la 
